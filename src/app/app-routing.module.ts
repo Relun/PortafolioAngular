4a22494fe0b6dad5e51f8ea7dd85d852e38cfb5d
Relun <claudia.araya.ro@gmail.com>
2019-09-02 17:53:21 -0400

    definiendo el contenido de cada componente, agregando animación en home con presentación de mí,componente about con leve reseña de mí y las habilidades técnicas,componente projects con cards con imagen y breve reseña de 3 projectos, componente contact incluirá informació de contacto. AUN FALTA DEFINIR COLORES A USAR EN PORTFOLIO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
-
-
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
 const routes: Routes = [
 
+  {path:'home', component:HomeComponent},
   {path:'navbar', component:NavbarComponent},
   {path:'about', component:AboutComponent},
   {path:'projects', component:ProjectsComponent},
+  {path:'contact', component:ContactComponent},
 ];
 
 @NgModule({
