fd921e994ad232d4121899d72c67cbd0d08ab0f0
Relun <claudia.araya.ro@gmail.com>
2019-09-29 23:55:19 -0300

    cambiando color de fondo, cambiando imagen de inicio por un parrafo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,11 @@ import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
+
 const routes: Routes = [
 
+  // { path: '', redirectTo: '/landing', pathMatch:'full' },
+  {path:'', component:HomeComponent},
   {path:'home', component:HomeComponent},
   {path:'navbar', component:NavbarComponent},
   {path:'about', component:AboutComponent},
