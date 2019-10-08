7f5115ede63e9a9adff970f0a7883cc6f7e4832a
Relun <claudia.araya.ro@gmail.com>
2019-10-08 00:33:09 -0300

    cambiando color de fondo de los disintos componentes, alineando la animacion, cambiando estilo de botones proyecto e inicio, navbar responsivo

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+ 
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
@@ -13,3 +14,12 @@ export class HomeComponent implements OnInit {
   }
 
 }
+
+// export class HomeComponent implements AfterViewInit {
+//   constructor(private elementRef: ElementRef){
+
+//   }
+//   ngAfterViewInit(){
+//     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'black';
+//  }
+// }
